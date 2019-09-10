8c4afe0ce13b0143adcf589b82b47fab958856a4
Bruno Vitorino <bruno@sumocoders.be>
2019-09-10 17:33:45 +0200

    Add deployer configuration

---
+++ deploy.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/symfony4.php';
+require 'recipe/cachetool.php';
+require 'recipe/sentry.php';
+require __DIR__ . '/vendor/tijsverkoyen/deployer-sumo/sumo.php';
+
+// Define some variables
+set('client', '$client');
+set('project', '$project');
+set('repository', '$repository');
+set('production_url', '$productionUrl');
+set('sentry_organization', '$sentryOrganization');
+set('sentry_project_slug', '$sentryProjectSlug');
+set('sentry_token', '$sentryToken');
+
+// Define staging
+host('$host')
+    ->user('sites')
+    ->stage('staging')
+    ->set('deploy_path', '~/apps/{{client}}/{{project}}')
+    ->set('branch', 'staging')
+    ->set('bin/php', '$phpBinary')
+    ->set('cachetool', '$socketPath')
+    ->set('document_root', '~/php72/{{client}}/{{project}}');
+
+// Define production
+//host('$host')
+//    ->user('sites')
+//    ->stage('production')
+//    ->set('deploy_path', '~/apps/{{client}}/{{project}}')
+//    ->set('branch', 'master')
+//    ->set('bin/php', '$phpBinary')
+//    ->set('cachetool', '$socketPath')
+//    ->set('document_root', '~/php72/{{client}}/{{project}}');
+
+/*************************
+ * No need to edit below *
+ *************************/
+
+set('use_relative_symlink', false);
+
+// Shared files/dirs between deploys
+add('shared_files', ['.env.local']);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+// Disallow stats
+set('allow_anonymous_stats', false);
+
+// Sentry
+set(
+    'sentry',
+    [
+        'organization' => get('sentry_organization'),
+        'projects' => [
+            get('sentry_project_slug'),
+        ],
+        'token' => get('sentry_token')
+    ]
+);
+
+/*****************
+ * Task sections *
+ *****************/
+// Build tasks
+task(
+    'build:assets:npm',
+    function () {
+        run('npm run build');
+    }
+)
+    ->desc('Run the build script which will build our needed assets.')
+    ->local();
+
+// Upload tasks
+task(
+    'upload:assets',
+    function () {
+        upload(__DIR__ . '/public/build', '{{release_path}}/public');
+    }
+)
+    ->desc('Uploads the assets')
+    ->addBefore('build:assets:npm');
+after('deploy:update_code', 'upload:assets');
+
+/**********************
+ * Flow configuration *
+ **********************/
+// Clear the Opcache
+after('deploy:symlink', 'cachetool:clear:opcache');
+// Unlock the deploy when it failed
+after('deploy:failed', 'deploy:unlock');
+// Migrate database before symlink new release.
+before('deploy:symlink', 'database:migrate');
