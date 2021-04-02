3b1cd834804d7ea16ac563a3acae05a1a9321ed6
Bjorn Van Acker <bjorn@sumocoders.be>
2021-04-02 11:19:22 +0200

    Allow testing for loggedin users

--- tests/PagesAreFoundTest.php
+++ tests/PagesAreFoundTest.php
@@ -2,21 +2,53 @@
 
 namespace App\Tests;
 
+//use App\Repository\UserRepository;
+use Symfony\Component\Panther\Client;
 use Symfony\Component\Panther\PantherTestCase;
 
 class PagesAreFoundTest extends PantherTestCase
 {
-    private const ROUTES = [
-        '/',
-    ];
+    private const LOGGED_IN_EMAIL = 'accounts@sumocoders.be';
 
-    public function testPagesAreFound()
+    /**
+     * @dataProvider providePublicUrls
+     */
+    public function testPublicPagesAreFound($route): void
     {
         $client = static::createPantherClient();
+        $client->request('GET', $route);
+        self::assertEquals(200, $client->getInternalResponse()->getStatusCode());
+    }
 
-        foreach ($this::ROUTES as $route) {
-            $client->request('GET', $route);
-            self::assertEquals(200, $client->getInternalResponse()->getStatusCode());
-        }
+    /**
+     * @dataProvider provideLoggedInUrls
+     */
+    public function testLoggedInPagesAreFound($route): void
+    {
+        $client = static::createPantherClient();
+        $this->loggin($client);
+        $client->request('GET', $route);
+        self::assertEquals(200, $client->getInternalResponse()->getStatusCode());
+    }
+
+    public function providePublicUrls()
+    {
+        return [
+            ['/']
+        ];
+    }
+
+    public function provideLoggedInUrls()
+    {
+        return [
+            ['/']
+        ];
+    }
+
+    private function loggin(Client $client): void
+    {
+        // $userRepository = static::$container->get(UserRepository::class);
+        // $user = $userRepository->findOneByEmail($this:LOGGED_IN_EMAIL);
+        // $client->loginUser($user);
     }
 }
