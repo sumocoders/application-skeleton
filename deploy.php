53c2c078dfdb510db6dd3a16c7c076aaff4519cf
Sander De la Marche <sander@sumocoders.be>
2021-09-25 13:43:23 +0200

    Finalize the Deployer 7 default deploy.php

--- deploy.php
+++ deploy.php
@@ -12,6 +12,7 @@ set('project', '$project');
 set('repository', '$repository');
 set('production_url', '$productionUrl');
 set('production_user', '$productionUser');
+set('php_version', '8.0');
 
 // Define staging
 host('dev02.sumocoders.eu')
@@ -19,24 +20,33 @@ host('dev02.sumocoders.eu')
     ->set('labels', ['stage' => 'staging'])
     ->set('deploy_path', '~/apps/{{client}}/{{project}}')
     ->set('branch', 'staging')
-    ->set('bin/php', 'php8.0')
-    ->set('document_root', '~/php74/{{client}}/{{project}}');
+    ->set('bin/php', '{{php_binary}}')
+    ->set('cachetool', '/var/run/php_{{php_version_numeric}}_fpm_sites.sock')
+    ->set('document_root', '~/php{{php_version_numeric}}/{{client}}/{{project}}');
 
 // Define production
 //host('$host')
-//    ->setRemoteUser(get('production_user'))
+//    ->setRemoteUser('{{production_user}}')
 //    ->set('labels', ['stage' => 'production'])
 //    ->port(2244)
 //    ->set('deploy_path', '~/wwwroot')
 //    ->set('branch', 'master')
-//    ->set('bin/php', 'php8.0')
+//    ->set('bin/php', '{{php_binary}}')
 //    ->set('document_root', '~/wwwroot/www')
-//    ->set('http_user', get('production_user'))
+//    ->set('http_user', '{{production_user}}')
 //    ->set('writable_mode', 'chmod'); Cloudstar only
 
 /*************************
  * No need to edit below *
  *************************/
+
+set('php_binary', function () {
+    return 'php' . get('php_version');
+});
+
+set('php_version_numeric', function () {
+    return (int) filter_var(get('bin/php'), FILTER_SANITIZE_NUMBER_INT);
+});
 
 set('use_relative_symlink', false);
 
@@ -50,7 +60,12 @@ add('writable_dirs', []);
 // Disallow stats
 set('allow_anonymous_stats', false);
 
-// Composer
+/*
+ * Composer
+ * Deployer also has this download&run snippet in its core, but they only use it
+ * when a global `composer` option isn't available. We always want to use the
+ * phar version from shared so we overwrite the default behaviour here.
+ */
 set('shared_folder', '{{deploy_path}}/shared');
 set('bin/composer', function () {
     if (!test('[ -f {{shared_folder}}/composer.phar ]')) {
@@ -58,8 +73,6 @@ set('bin/composer', function () {
     }
     return '{{bin/php}} {{shared_folder}}/composer.phar';
 });
-
-set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
 
 /*****************
  * Task sections *
