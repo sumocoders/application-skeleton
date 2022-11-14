60e302c61e0b6db510591412ca6a07e95a09918e
Sander De la Marche <sander@sumocoders.be>
2022-11-14 15:31:37 +0100

    Replace hardcoded newlines with OS agnostic PHP_EOL constant

--- src/Skeleton/PostCreateProject.php
+++ src/Skeleton/PostCreateProject.php
@@ -110,11 +110,11 @@ class PostCreateProject
         ];
         $matches = [];
         preg_match('|import\s.*styles\/app\.(s)?css|', $content, $matches, PREG_OFFSET_CAPTURE);
-        $offset = mb_strpos($content, "\n", $matches[0][1]);
+        $offset = mb_strpos($content, PHP_EOL, $matches[0][1]);
         $content = self::insertStringAtPosition(
             $content,
             $offset,
-            "\n" . implode("\n", $insert)
+            PHP_EOL . implode(PHP_EOL, $insert)
         );
 
         if ($io->isVerbose()) {
@@ -133,7 +133,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             mb_strlen($content),
-            "\n" . implode("\n", $insert) . "\n"
+            PHP_EOL . implode(PHP_EOL, $insert) . PHP_EOL
         );
 
         // store the file
@@ -176,7 +176,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             0,
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
         $io->notice('→ remove useless entries');
@@ -192,7 +192,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             self::findEndOfEncoreEntries($content),
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
         $io->notice('→ enable Sass/SCSS support');
@@ -211,7 +211,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             self::findEndOfEncoreConfiguration($content),
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
 
@@ -225,7 +225,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             self::findEndOfEncoreConfiguration($content),
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
 
@@ -243,7 +243,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             self::findEndOfEncoreConfiguration($content),
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
 
@@ -259,7 +259,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             self::findEndOfEncoreConfiguration($content),
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
         $io->notice('→ add Vue loader');
@@ -269,7 +269,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             self::findEndOfEncoreConfiguration($content),
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
 
@@ -286,7 +286,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             $matches[0][1],
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
 
 
@@ -374,7 +374,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             $offset,
-            "\n" . implode("\n", $insert) . "\n"
+            PHP_EOL . implode(PHP_EOL, $insert) . PHP_EOL
         );
         file_put_contents($projectDir . '/config/packages/twig.yaml', $content);
 
@@ -383,7 +383,7 @@ class PostCreateProject
         $content = file_get_contents($projectDir . '/config/services.yaml');
         $matches = [];
         preg_match('|parameters:|', $content, $matches, PREG_OFFSET_CAPTURE);
-        $offset = mb_strpos($content, "\n", $matches[0][1]) + 1;
+        $offset = mb_strpos($content, PHP_EOL, $matches[0][1]) + 1;
         $insert = [
             '  # configuration of the locale, used for url and allowed locales',
             '  locale: \'nl\'',
@@ -405,7 +405,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             $offset,
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
         file_put_contents($projectDir . '/config/services.yaml', $content);
 
@@ -422,7 +422,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             $offset,
-            "\n" . implode("\n", $insert)
+            PHP_EOL . implode(PHP_EOL, $insert)
         );
         file_put_contents($projectDir . '/config/routes.yaml', $content);
 
@@ -438,7 +438,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             $offset,
-            "\n" . implode("\n", $insert) . "\n"
+            PHP_EOL . implode(PHP_EOL, $insert) . PHP_EOL
         );
         file_put_contents($projectDir . '/config/packages/framework.yaml', $content);
 
@@ -460,7 +460,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             mb_strlen($content) + 1,
-            implode("\n", $insert) . "\n"
+            implode(PHP_EOL, $insert) . PHP_EOL
         );
         file_put_contents($projectDir . '/config/packages/sentry.yaml', $content);
 
@@ -493,7 +493,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             $offset,
-            "\n" . implode("\n", $insert) . "\n"
+            PHP_EOL . implode(PHP_EOL, $insert) . PHP_EOL
         );
         file_put_contents($projectDir . '/config/packages/doctrine_migrations.yaml', $content);
 
@@ -510,7 +510,7 @@ class PostCreateProject
         $content = self::insertStringAtPosition(
             $content,
             mb_strlen($content),
-            "\n" . implode("\n", $insert)
+            PHP_EOL . implode(PHP_EOL, $insert)
         );
         file_put_contents($projectDir . '/.env', $content);
     }
