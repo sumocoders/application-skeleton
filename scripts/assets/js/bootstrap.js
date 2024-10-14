e135e84c15ee3d5f91c6f5da9f6c373e2b9b9af3
Jonas De Keukelaere <jonas@sumocoders.be>
2024-10-14 14:27:54 +0100

    Add sidebar collapsable stimulus controller

--- scripts/assets/js/bootstrap.js
+++ scripts/assets/js/bootstrap.js
@@ -1,7 +1,9 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 import Clipboard from '@stimulus-components/clipboard';
+import SidebarCollapsable from 'sumocoders/SidebarCollapsable';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
 app.register('clipboard', Clipboard);
+app.register('sidebar-collapsable', SidebarCollapsable);
