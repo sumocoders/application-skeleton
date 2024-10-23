9686cc9440daab5f7ad4e6dc9e6497400e03fba9
Jonas De Keukelaere <jonas@sumocoders.be>
2024-10-23 09:31:39 +0100

    standardjs

--- scripts/assets/js/bootstrap.js
+++ scripts/assets/js/bootstrap.js
@@ -1,27 +1,27 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-import Clipboard from 'sumocoders/Clipboard';
-import SidebarCollapsable from 'sumocoders/SidebarCollapsable';
-import Toast from 'sumocoders/Toast';
-import Theme from 'sumocoders/Theme';
-import Tooltip from 'sumocoders/Tooltip';
-import Popover from 'sumocoders/Popover';
-import DateTimePicker from 'sumocoders/DateTimePicker';
-import Tabs from 'sumocoders/Tabs';
-import PasswordStrengthChecker from 'sumocoders/PasswordStrengthChecker';
-import FormCollection from 'sumocoders/FormCollection';
-import ScrollToTop from 'sumocoders/ScrollToTop';
+import { startStimulusApp } from '@symfony/stimulus-bundle'
+import Clipboard from 'sumocoders/Clipboard'
+import SidebarCollapsable from 'sumocoders/SidebarCollapsable'
+import Toast from 'sumocoders/Toast'
+import Theme from 'sumocoders/Theme'
+import Tooltip from 'sumocoders/Tooltip'
+import Popover from 'sumocoders/Popover'
+import DateTimePicker from 'sumocoders/DateTimePicker'
+import Tabs from 'sumocoders/Tabs'
+import PasswordStrengthChecker from 'sumocoders/PasswordStrengthChecker'
+import FormCollection from 'sumocoders/FormCollection'
+import ScrollToTop from 'sumocoders/ScrollToTop'
 
-const app = startStimulusApp();
+const app = startStimulusApp()
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
-app.register('clipboard', Clipboard);
-app.register('sidebar-collapsable', SidebarCollapsable);
-app.register('toast', Toast);
-app.register('theme', Theme);
-app.register('tooltip', Tooltip);
-app.register('popover', Popover);
-app.register('date-time-picker', DateTimePicker);
-app.register('tabs', Tabs);
-app.register('password-strength-checker', PasswordStrengthChecker);
-app.register('form-collection', FormCollection);
-app.register('scroll-to-top', ScrollToTop);
+app.register('clipboard', Clipboard)
+app.register('sidebar-collapsable', SidebarCollapsable)
+app.register('toast', Toast)
+app.register('theme', Theme)
+app.register('tooltip', Tooltip)
+app.register('popover', Popover)
+app.register('date-time-picker', DateTimePicker)
+app.register('tabs', Tabs)
+app.register('password-strength-checker', PasswordStrengthChecker)
+app.register('form-collection', FormCollection)
+app.register('scroll-to-top', ScrollToTop)
