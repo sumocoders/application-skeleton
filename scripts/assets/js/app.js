babbf456feee7f498b83235905f2c53ef8a10e57
Tijs Verkoyen <tijs@verkoyen.eu>
2025-08-11 11:29:34 +0200

    (feat) Add CSP Nonce header for all fetch requests

--- scripts/assets/js/app.js
+++ scripts/assets/js/app.js
@@ -5,6 +5,12 @@ import 'tom-select/dist/css/tom-select.bootstrap5.css'
 import 'flatpickr/dist/flatpickr.css'
 import 'flatpickr/dist/themes/airbnb.css'
 
+// Fix CSP Nonce for Turbo requests
+// See: https://github.com/hotwired/turbo/issues/294#issuecomment-877842232
+document.addEventListener('turbo:before-fetch-request', (event) => {
+  event.detail.fetchOptions.headers['X-CSP-Nonce'] = document.querySelector('meta[name="csp-nonce"]').content
+})
+
 // JS
 import 'bootstrap'
 import './bootstrap.js'
